b780c57605750b3ace9f7ab64b7ec2f5eff7380c
Posh <posh@Mac.fritz.box>
2026-01-29 15:18:25 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,7 +2,7 @@ import json
 
 def load_data(file_path):
   """ Loads a JSON file """
-  with open(file_path, "r") as handle:
+  with open(file_path, "r", encoding="utf-8") as handle:
     return json.load(handle)
 
 animals_data = load_data('animals_data.json')
@@ -12,30 +12,32 @@ for animal in animals_data:
     output += '<li class="cards__item">'
     # Name prüfen und ausgeben
     if "name" in animal:
-        output += f"Name: {animal['name']}<br/>\n"
+        output += f"  <div class='card__title'>{animal['name']}</div>\n"
+
+    output += '  <p class="card__text">\n'
 
     #Diet prüfen und Achtung! *(liegt innerhalb von 'characteristics')
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}<br/>\n" #<--- *
+        output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n" #<--- *
 
     #Location prüfen *(erstes Element der Liste 'locations')
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f"Location: {animal['locations'][0]}<br/>\n" #* [0] = erstes Element
+        output += f"<strong>Location:</strong> {animal['locations'][0]}<br/>\n" #* [0] = erstes Element
 
     #Type prüfen (liegt auch in 'characteristics')
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}<br/>\n"
+        output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
 
-    output += '</li>'
+    output += '</li>\n'
 
 
 
 print(output)
 
-with open("animals_template.html", "r") as f:
+with open("animals_template.html", "r", encoding="utf-8") as f:
     template_content = f.read()
 
 new_html_content = template_content.replace("__REPLACE_ANIMALS_INFO__", output)
 
-with open("animals.html", "w") as f:
+with open("animals.html", "w", encoding="utf-8") as f:
     f.write(new_html_content)
