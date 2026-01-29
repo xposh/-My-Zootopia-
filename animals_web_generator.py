6c84e099524f9bb5377215ccb67b19b806799d8b
Posh <posh@Mac.fritz.box>
2026-01-29 14:50:03 +0100

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -9,23 +9,24 @@ animals_data = load_data('animals_data.json')
 output = ""
 
 for animal in animals_data:
+    output += '<li class="cards__item">'
     # Name prüfen und ausgeben
     if "name" in animal:
-        output += f"Name: {animal['name']}\n"
+        output += f"Name: {animal['name']}<br/>\n"
 
     #Diet prüfen und Achtung! *(liegt innerhalb von 'characteristics')
     if "characteristics" in animal and "diet" in animal["characteristics"]:
-        output += f"Diet: {animal['characteristics']['diet']}\n" #<--- *
+        output += f"Diet: {animal['characteristics']['diet']}<br/>\n" #<--- *
 
     #Location prüfen *(erstes Element der Liste 'locations')
     if "locations" in animal and len(animal["locations"]) > 0:
-        output += f"Location: {animal['locations'][0]}\n" #* [0] = erstes Element
+        output += f"Location: {animal['locations'][0]}<br/>\n" #* [0] = erstes Element
 
     #Type prüfen (liegt auch in 'characteristics')
     if "characteristics" in animal and "type" in animal["characteristics"]:
-        output += f"Type: {animal['characteristics']['type']}\n"
+        output += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-    output += "\n"
+    output += '</li>'
 
 
 
